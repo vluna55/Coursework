a141d71f7f22022616fbaa55131ba0b54fc62aea
vluna55 <vincentluna70@yahoo.com>
2023-09-05 18:27:14 -0500

    block07

---
+++ block07/main.js
@@ -0,0 +1,19 @@
+/*pseudocode: 
+I want to declare three variables for my combination
+Each variable is an equation that leads to the correct number in the combination
+After that i add the strings together so it can display all three numbers of the combination
+*/
+const greeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+
+console.log(greeting);
+
+//Created 3 variables with different equations
+const str1 = 2 * 5;
+const str2 = 80 / 2;
+const str3 = 30 + 9;
+
+
+//Created a function to make the alert pop up on the html file
+function myFunction() {
+    alert(str1 + " " + str2 + " " + str3);
+}
